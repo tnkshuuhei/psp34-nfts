3b2935730e4246f904e03df2d0c663fce81569c3
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-31 16:58:23 +0900

    add header

---
+++ component/Header.js
@@ -0,0 +1,63 @@
+import React, { useContext } from 'react'
+import Image from "next/image"
+import { AiOutlineDown } from 'react-icons/ai'
+import Shiden from '../assets/Shiden.png'
+import { ConnectContext } from '../context/ConnectProvider'
+//import { Identicon } from '@polkadot/react-identicon'
+
+const style = {
+	wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+	title: 'text-4xl text-green-700 text-center font-semibold',
+	headwrapper: `p-4 w-screen flex justify-between items-center`,
+	headerLogo: `flex w-1/4 items-center justify-start`,
+	buttonsContainer: `flex w-1/4 justify-end items-center`,
+	button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
+	buttonPadding: `p-2`,
+	buttonIconContainer: `flex items-center justify-center w-8 h-8`,
+}
+function Header() {
+	const { connectWallet, currentAccount, api } = useContext(ConnectContext)
+	return (
+		<div className={style.wrapper}>
+			<div className={style.headwrapper}>
+				<div className={style.title}>Mint Shiden NFTs</div>
+				<div className={style.buttonsContainer}>
+					<div className={`${style.button} ${style.buttonPadding}`}>
+						<div className={style.buttonIconContainer}>
+							<Image src={Shiden} alt='shiden' height={20} width={20} />
+						</div>
+						<p>Shiden</p>
+						<div className={style.buttonIconContainer}>
+							<AiOutlineDown />
+						</div>
+					</div>
+					{currentAccount ? (
+						<div className={`${style.button} ${style.buttonPadding}`}>
+							<div className={style.buttonIconContainer}>
+								{/**
+								<Identicon
+									value={currentAccount.address}
+									size={20}
+									theme={'polkadot'}
+								/>
+								 */}
+							</div>
+							<div className={style.buttonTextContainer}>{currentAccount.meta.name}</div>
+						</div>
+					) : (
+						<div
+							onClick={() => connectWallet()}
+							className={`${style.button} ${style.buttonPadding}`}
+						>
+							<div className={`${style.buttonAccent} ${style.buttonPadding}`}>
+								Connect Wallet
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
-import Head from 'next/head'
 import Image from 'next/image'
-import Button from '../component/Button'
-import styles from '../styles/Home.module.css'
+//import Button from '../component/Button'
+import Header from '../component/Header'
+//import styles from '../styles/Home.module.css'
 import { ConnectContext } from '../context/ConnectProvider'
 import { useContext } from 'react'
+
 export default function Home() {
 	const { connectWallet, currentAccount, api } = useContext(ConnectContext)
 	return (
-		<div className={styles.container}>
-			<div onClick={() => connectWallet()}>
-				<Button></Button>
+		<div>
+			<Header />
+			<div >
 			</div>
-			<div className={styles.footer}
-				onClick={() => connectWallet()}>
-				<span className={styles.logo}>
-					<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-				</span>
-			</div>
+
 		</div>
 	)
 }
