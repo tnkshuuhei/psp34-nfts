8867db36e6d5f1b70d568f26a48925ec8d3c73e6
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-10-31 17:36:40 +0900

    setup css

--- components/Main.js
+++ components/Main.js
@@ -1,98 +1,56 @@
 import Image from 'next/image'
 import { RiSettings3Fill } from 'react-icons/ri'
-import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai'
-import { IoSwapVertical } from 'react-icons/io5'
-import shiden from '../assets/Shiden.png'
 import { useState } from 'react'
 import Button from './Button'
+
 const style = {
 	wrapper: `w-screen flex items-center justify-center mt-14`,
 	content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
 	formHeader: `px-2 flex items-center justify-between font-semibold text-xl`,
-	transferPropContainer: `bg-[#20242A] my-3 rounded-2xl p-6 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
+	transferPropContainer: `bg-[#20242A] my-4 rounded-2xl p-4 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
 	transferPropInput: `bg-transparent placeholder:text-[#B2B9D2] outline-none mb-6 w-full text-2xl`,
-	currencySelector: `flex w-1/4`,
-	currencySelectorContent: `w-full h-min flex justify-between items-center bg-[#2D2F36] hover:bg-[#41444F] rounded-2xl text-xl font-medium cursor-pointer p-2 mt-[-0.2rem]`,
-	currencySelectorIcon: `flex items-center`,
-	swapIcon: `flex items-center justify-center`,
-	currencySelectorTicker: `mx-2`,
-	currencySelectorArrow: `text-lg`,
+
+
 }
 const Main = () => {
-	const [showList, setShowList] = useState(false);
-
+	const [Name, setName] = useState();
+	const [Image, setImage] = useState();
+	const [Description, setDescription] = useState();
 	return (
 		<div className={style.wrapper}>
 			<div className={style.content}>
 				<div className={style.formHeader}>
-					<div>Swap</div>
+					<div>Setup</div>
 					<div>
 						<RiSettings3Fill />
 					</div>
 				</div>
 				<div className={style.transferPropContainer}>
 					<input
-						type='text'
+						type='file'
 						className={style.transferPropInput}
-						placeholder='0.0'
-						pattern='^[0-9]*[.,]?[0-9]*$'
-						onChange={e => handleChange(e, 'amount')}
-					/>
-					<div className={style.currencySelector}>
-						<button className={style.currencySelectorContent} onClick={() => setShowList
-							((prevState) => !prevState)}>
-							<div className={style.currencySelectorTicker}>SDN</div>
-							{showList ? (
-								<AiOutlineUp className={style.currencySelectorArrow} />
-							) : (
-								<AiOutlineDown className={style.currencySelectorArrow} />
-							)}
-						</button>
-						{/**
-			  {showList && (
-					<ul className={style.currencySelectorContent}>
-					{[
-						{token:'aUSD', tokenName:'aUSD'},
-						{token:'USDT', tokenName:'USDT'},
-						].map(({token,tokenName},index) => (
-							<li
-								key={index}
-								className={`${style.currencySelectorTicker} `}
-							>
-							{tokenName}
-							</li>
-						))}
-					</ul>
-				)} 
-				*/}
-					</div>
-				</div>
-				<div className={style.swapIcon}>
-					<IoSwapVertical
-						size={42}
+						placeholder='Upload Image File for NFT'
+						onChange={e => setImage(e.target.value)}
 					/>
 				</div>
-
 				<div className={style.transferPropContainer}>
 					<input
 						type='text'
 						className={style.transferPropInput}
-						placeholder='0.0'
-						pattern='^[0-9]*[.,]?[0-9]*$'
-						onChange={e => handleChange(e, 'amount')}
+						placeholder='NFTs Name'
+						onChange={e => setName(e.target.value)}
 					/>
-					<div className={style.currencySelector}>
-						<button className={style.currencySelectorContent}>
-							<div className={style.currencySelectorTicker}>USDT</div>
-							{showList ? (
-								<AiOutlineUp className={style.currencySelectorArrow} />
-							) : (
-								<AiOutlineDown className={style.currencySelectorArrow} />
-							)}
-						</button>
-					</div>
 				</div>
-				<Button title='confirm' />
+				<div className={style.transferPropContainer}>
+					<input
+						type='text'
+						className={style.transferPropInput}
+						placeholder='Description'
+						onChange={e => setDescription(e.target.value)}
+					/>
+				</div>
+
+				<Button title='Mint' />
 			</div>
 
 		</div>
