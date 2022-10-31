17b139f868207b25ae46985f521517639b3c45e4
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2022-11-01 00:03:15 +0900

    gather center

--- components/Main.js
+++ components/Main.js
@@ -6,7 +6,7 @@ import { ABI, CONTRACT_ADDRESS } from './constants'
 import { ContractPromise } from '@polkadot/api-contract'
 
 const style = {
-	wrapper: `w-screen flex items-center justify-center mt-14`,
+	wrapper: `h-screen w-screen flex items-center justify-center mt-14`,
 	content: `bg-[#191B1F] w-[40rem] rounded-2xl p-4`,
 	formHeader: `px-2 flex items-center justify-between font-semibold text-xl`,
 	transferPropContainer: `bg-[#20242A] my-4 rounded-2xl p-4 text-3xl  border border-[#20242A] hover:border-[#41444F]  flex justify-between`,
